db8c55e82b68b207aba3f2d2c0d68b9e7535cb94
unknown <angelesianlloyd_bist@gmail.com>
2025-02-20 18:40:06 +0800

    Submitting Python Week 04 - Laboratory # 14

---
+++ nested_for_loop_multiplication_table.py
@@ -0,0 +1,16 @@
+# Iver John R Monroy
+# ITELEC2
+# Laboratory #14 -Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conversations)
+
+def main():
+
+    for i in range(1, 6):
+        for j in range(1, 6):  # Indent this loop!
+            product = i * j  # Indent this line!
+            print(f"{product:4}", end="")  # Indent this line!
+
+        print()
+
+if __name__ == "__main__":
+    main()
